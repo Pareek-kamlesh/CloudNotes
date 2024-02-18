139ffceff860e269f1195b5a90bca18ec52b4a98
Pareek-kamlesh <kamleshpareek21@gmail.com>
2024-02-18 23:11:41 +0530

    Step-3

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/Users')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt = require("bcryptjs")
+var fetchUser = require("../middleware/fetchUser")
 var jwt = require('jsonwebtoken')
 
 
@@ -40,9 +41,60 @@ router.post('/createUser',[body('name').isLength({min:3}), body('email').isEmail
     res.json(authToken)
     }catch(error){
         console.error(message.error)
+        res.status(500).send("Internal server error");
     }
      
     
 })
 
+router.post('/login',[
+    body('email').isEmail(), 
+    body('password').exists()],
+    async    (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please enter correct credentials"})
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please enter correct credentials"})
+        }
+
+        const data ={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        
+    
+        res.json(authToken)
+    } catch (error) {
+        console.error(message.error)
+        res.status(500).send("Internal server error");
+    }
+})
+
+router.post('/getUser', fetchUser,
+    async    (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    try {
+        userId=req.user.id
+        const user = await User.findById(userId).select("-password");
+        res.send(user)
+    } catch (error) {
+        console.error(message.error)
+        res.status(500).send("Internal server error");
+    }
+})
 module.exports=router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,48 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const fetchUser = require("../middleware/fetchUser");
+const router = express.Router();
+const Note = require("../models/Note");
+const { body, validationResult } = require('express-validator');
 
 
-router.get('/', (req, res, next)=>{
-   
-    res.json([])
-    next()
-})
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
-module.exports = router
+// Addng new note using POST
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    body("title").isLength({ min: 3 }),
+    body("description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savednote = await note.save();
+      res.json(savednote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+module.exports = router;
